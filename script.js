a4a0524c714b669785ca77eb2ce4f12b631d459c
beatrizrlopes <113106555+beatrizrlopes@users.noreply.github.com>
2024-08-23 11:19:28 -0300

    sscript

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const perguntas = [
     {
-        enunciado: ''Quais são os principais impactos da poluição marinha na vida marinha?'',
+        enunciado: "Quais são os principais impactos da poluição marinha na vida marinha?",
         alternativas:[
-            ''A poluição marinha pode causar a morte de animais marinhos devido à ingestão de plásticos e outros resíduos, que podem entupir seus sistemas digestivos ou liberar toxinas.
-            '' Ela também pode prejudicar a reprodução e o crescimento de organismos marinhos, como corais e peixes, afetando toda a cadeia alimentar e a biodiversidade dos oceanos.
+            "A poluição marinha pode causar a morte de animais marinhos devido à ingestão de plásticos e outros resíduos, que podem entupir seus sistemas digestivos ou liberar toxinas."
+            " Ela também pode prejudicar a reprodução e o crescimento de organismos marinhos, como corais e peixes, afetando toda a cadeia alimentar e a biodiversidade dos oceanos."
 
         ]
     },
     {
-        enunciado: ''Como os produtos químicos e os metais pesados afetam os oceanos?
+        enunciado: "Como os produtos químicos e os metais pesados afetam os oceanos?"
         alternativas: [
-            ''Produtos químicos, como pesticidas e fertilizantes, podem causar a eutrofização, levando ao crescimento excessivo de algas e à diminuição do oxigênio na água, o que prejudica a vida marinha.
-            '' Metais pesados, como mercúrio e chumbo, podem se acumular nos organismos marinhos e entrar na cadeia alimentar, afetando a saúde dos animais e, eventualmente, dos seres humanos que consomem esses produtos.
+            "Produtos químicos, como pesticidas e fertilizantes, podem causar a eutrofização, levando ao crescimento excessivo de algas e à diminuição do oxigênio na água, o que prejudica a vida marinha."
+            "Metais pesados, como mercúrio e chumbo, podem se acumular nos organismos marinhos e entrar na cadeia alimentar, afetando a saúde dos animais e, eventualmente, dos seres humanos que consomem esses produtos."
 
         ]
     },
     {
-        enunciado:Qual é o papel dos resíduos plásticos na poluição marinha?
+        enunciado:"Qual é o papel dos resíduos plásticos na poluição marinha?"
         alternativas: [
-            ''Os resíduos plásticos, como sacolas e garrafas, frequentemente acabam no oceano, onde podem ser ingeridos por animais marinhos, causando obstruções intestinais e morte.
-            ''Os plásticos também se fragmentam em microplásticos, que são ingeridos por uma ampla gama de organismos marinhos e podem causar danos internos, além de introduzir toxinas na cadeia alimentar.
+            "OOs plásticos também se fragmentam em microplásticos, que são ingeridos por uma ampla gama de organismos marinhos e podem causar danos internos, além de introduzir toxinas na cadeia alimentar.s resíduos plásticos, como sacolas e garrafas, frequentemente acabam no oceano, onde podem ser ingeridos por animais marinhos, causando obstruções intestinais e morte."
+            "Os plásticos também se fragmentam em microplásticos, que são ingeridos por uma ampla gama de organismos marinhos e podem causar danos internos, além de introduzir toxinas na cadeia alimentar."
 
         ]
     
